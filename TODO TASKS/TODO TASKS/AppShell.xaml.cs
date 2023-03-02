103f97e350ef328a30330bbf7ffd8ef39510b712
Termolk <91014529+Termolk@users.noreply.github.com>
2023-03-02 19:27:57 +0300

    initial commit

---
+++ TODO TASKS/TODO TASKS/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace TODO_TASKS;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ TODO TASKS/TODO TASKS/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace TODO_TASKS;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
