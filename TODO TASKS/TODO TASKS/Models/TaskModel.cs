45a0bea38db3d884267c5e031b5f37ef8a6c75fb
Termolk <91014529+Termolk@users.noreply.github.com>
2023-03-03 12:39:51 +0300

    Create a model for tasks

---
+++ TODO TASKS/TODO TASKS/Models/TaskModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TODO_TASKS.Models
+{
+    public class TaskModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime DueDate { get; set; }
+        public bool IsCompleted { get; set; }
+        public int Priority { get; set; }
+    }
+}
--- TODO TASKS/TODO TASKS/ViewModel/ViewModel.cs
+++ TODO TASKS/TODO TASKS/ViewModel/ViewModel.cs
@@ -1,12 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace TODO_TASKS.ViewModel
 {
-    internal class ViewModel
+    public class ViewModel : INotifyPropertyChanged
     {
+
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
